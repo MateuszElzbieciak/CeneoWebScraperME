92194acc842cb9d1802ef0dec8bf09925428d68c
MateuszElzbieciak <matix.elzbi@icloud.com>
2024-05-13 00:11:17 +0200

    2024-13-05 ME

--- app/routes.py
+++ app/routes.py
@@ -10,22 +10,14 @@ def index():
 def extract():
     return render_template("extract.html")
 
-
 @app.route('/products')
 def products():
     return render_template("products.html")
-
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-
 @app.route('/product/<product_id>')
-def product_id():
+def product(product_id):
     return render_template("product.html", product_id=product_id)
-
-
-@app.route('/name/<name>')
-def name():
-    return f"Hello, {name}!"
